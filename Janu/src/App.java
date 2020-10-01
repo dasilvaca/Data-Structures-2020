29afa5e3ad148f19cbaaacee172a854e3b74439e
dasilvaca <dssc1107@gmail.com>
2020-09-30 23:43:34 -0500

    Locuras en la noche

--- Janu/src/App.java
+++ Janu/src/App.java
@@ -1,7 +1,15 @@
-//import jdk.internal.org.jline.terminal.Size;
+//import structs.*;S
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Node a = new Node("xd");
         
+        Queue A = new Queue();
+        A.enqueue("ajksdfbjklsdaf");
+
+        A.print();
+
+
+
     }
 }
