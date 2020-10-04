c43b5b7181a839c7b03721d5cff83cf61acfde60
Juan Bedoya <54643704+jubedoyat@users.noreply.github.com>
2020-10-04 12:49:32 -0500

    La clase Stack está lista

--- src/structs/Stack.java
+++ src/structs/Stack.java
@@ -1,5 +1,66 @@
 package structs;
 
 public class Stack {
+    Node top;
     
+    public void Stack(){
+        this.top = null;
+    }
+    
+    public void push(Object newElement){
+        Node newNode = new Node(newElement);
+        if (this.top != null){
+            newNode.next = this.top;
+        }
+        this.top = newNode;
+    }
+    
+    public Object pop(){
+        Object returnElem = this.top.data;
+        this.top = this.top.next;
+        return returnElem;
+    }
+    
+    public Object peek(){
+        return this.top.data;
+    }
+    
+    public boolean isEmpty(){
+        if(this.top == null){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    
+    public int len(){
+        Node curr = this.top;
+        int len = 0;
+        while(curr != null){
+            len++;
+            curr = curr.next;
+        }
+        return len;
+    }
+    
+    public void makeEmpty(){
+        Node curr = this.top;
+        while(curr != null){
+            this.top = this.top.next;
+            curr = curr.next;
+        }
+    }
+    
+    public void print(){
+        Node curr = this.top;
+        System.out.print("[");
+        while(curr != null){
+            if(curr.next != null){
+                System.out.print(curr.data + ", ");
+            } else{
+                System.out.println(curr.data + "]");
+            }
+            curr = curr.next;
+        }
+    }
 }
