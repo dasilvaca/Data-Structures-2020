0b712a36f2b9a45f810798ce360a27929059eeda
jmorenohj <jmorenoh@unal.edu.co>
2020-10-12 21:42:07 -0500

    Por alguna razon se desaparecieron las queues asi que cree una con nodos doblemente enlazados, lo hice porque en el taller toca implementar queues asi que si deciden usarlas espero que les sirva

--- src/App.java
+++ src/App.java
@@ -12,9 +12,30 @@ import structs.*;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        
-        
+        DoubleQueue<String> hola= new DoubleQueue<String>();
+        System.out.println(hola.Size());
+        hola.put("Hola");
+        System.out.println(hola.Size());
+        hola.print();
+        System.out.println(hola.Size());
+        hola.put("Me");
+        System.out.println(hola.Size());
+        hola.print();
+        System.out.println(hola.Size());
+        hola.put("Llamo");
+        System.out.println(hola.Size());
+        hola.print();
+        System.out.println(hola.Size());
+        hola.put("Jose");
+        System.out.println(hola.Size());
+        hola.print();
 
+        System.out.println(hola.remove());
+        System.out.println(hola.Size());
+        System.out.println(hola.remove());
+        System.out.println(hola.Size());
+        hola.print();
+        
 
 
         
---
+++ src/structs/DoubleQueue.java
@@ -0,0 +1,52 @@
+package structs;
+
+public class DoubleQueue<T>{
+    public DoubleNode<T> FirstIn;
+    public DoubleNode<T> LastIn;
+    int size;
+
+    public DoubleQueue(){
+        size=0;
+        FirstIn=null;
+        LastIn=null;
+    }
+
+    public boolean isEmpty(){
+        if(size==0){
+            return true;
+        }
+        return false;
+    }
+    public void put(T data){
+        DoubleNode<T> newNode = new DoubleNode<T>(data);
+        if(size==0){
+            FirstIn=LastIn=newNode;
+            size=1;
+        }else{
+            LastIn.back=newNode;
+            newNode.next=LastIn;
+            LastIn=newNode;
+            size+=1;
+        }
+    }
+    public int Size(){
+        return size;
+    }
+    public T remove(){
+        T data=FirstIn.data;
+        FirstIn=FirstIn.back;
+        FirstIn.next=null;
+        size-=1;
+        return data;
+    }
+    public void print(){
+        DoubleNode<T> iterador=LastIn;
+        String imprimible="[ ";
+        while(iterador!=null){
+            imprimible+=iterador.data.toString()+" ";
+            iterador=iterador.next;
+        }
+        System.out.println(imprimible+"]");
+    }
+
+}
