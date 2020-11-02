7903a4c6f70801864475035cb5d50994be70b825
jmorenohj <jmorenoh@unal.edu.co>
2020-11-02 10:24:32 -0500

    .

--- src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.janus.janusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 //public static boolean opened = false;
 public class MainActivity extends AppCompatActivity {
-
+    TextView prueba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prueba=(TextView)findViewById(R.id.prueba);
+
     }
 }
