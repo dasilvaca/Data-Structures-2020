606d83f07888728fd878f0dc2c02cd948d2a90a0
jmorenohj <jmorenoh@unal.edu.co>
2020-11-02 17:16:27 -0500

    necesito hacer pull

--- src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
@@ -3,20 +3,23 @@ package com.janus.janusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.janus.janusapp.structs.*;
 
 //public static boolean opened = false;
 public class MainActivity extends AppCompatActivity {
     TextView prueba;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
         setContentView(R.layout.activity_main);
-        prueba=(TextView)findViewById(R.id.prueba);
 
-=======
-        setContentView(R.layout.login_screen);
->>>>>>> 64f47c8e6231a7ae73cc4b6168f259497a99a06e
+
+
+
+
     }
 }
