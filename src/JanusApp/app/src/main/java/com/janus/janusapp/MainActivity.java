4ff2f38dfe1b328139c8816ff08f7cd0485cf667
jmorenohj <jmorenoh@unal.edu.co>
2020-11-04 13:41:52 -0500

    Puse mas elementos en el bottom view

--- src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.janus.janusapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 //public static boolean opened = false;
 public class MainActivity extends AppCompatActivity {
@@ -18,15 +24,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
 
-        //setContentView(R.layout.activity_main);
-        //prueba=(TextView)findViewById(R.id.prueba);
+        BottomNavigationView bottomnav = findViewById(R.id.bottomNavigationView2);
 
-        setContentView(R.layout.login_layout);
+        bottomnav.setOnNavigationItemSelectedListener(navListener);
+
     }
-    public void change(View v){
-        Intent i = new Intent(this,Inicio.class);
-        startActivity(i);
-    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch(item.getItemId()){
+                        case R.id.configFragment:
+                            selectedFragment=new configFragment();
+                            break;
+                        case R.id.homeFragment:
+                            selectedFragment=new homeFragment();
+                            break;
+                        case R.id.newProjectFragment:
+                            selectedFragment=new newProjectFragment();
+                            break;
+                        case R.id.profileFragment:
+                            selectedFragment=new profileFragment();
+                            break;
+                        case R.id.searchFragment:
+                            selectedFragment=new searchFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment,selectedFragment).commit();
+                    return true;
+                }
+            };
 
 }
