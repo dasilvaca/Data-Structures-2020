22dc0e094c69157557e5b41ec57a4ba6113f2e62
jmorenohj <jmorenoh@unal.edu.co>
2020-11-04 19:18:32 -0500

    Cree las activitys para los usuarios y los proyectos.

---
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/Project.java
@@ -0,0 +1,14 @@
+package com.janus.janusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Project extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }
+}
