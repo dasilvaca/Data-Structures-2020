579b334753679d9fb623f3f5c076a71c614491c4
andgonzalezcas <54643793+andgonzalezcas@users.noreply.github.com>
2020-11-08 21:29:50 -0500

    Se añade imagenes para profile, no es el diseñor definitivo

--- src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/MainActivity.java
@@ -4,13 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.view.MenuItem;
 import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.janus.janusapp.structs.AVLTree;
-
 
 //public static boolean opened = false;
 public class MainActivity extends AppCompatActivity {
