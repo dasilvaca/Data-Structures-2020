bc627e904e9240f2f4d1d477d33127763a8400d7
dasilvaca <dasilvaca@unal.edu.co>
2020-11-09 21:08:41 -0500

    Prueberijillas con Juan Eduardo nocturnas xd

--- src/JanusApp/app/src/main/java/com/janus/janusapp/classes/User.java
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/classes/User.java
@@ -8,53 +8,58 @@ package com.janus.janusapp.classes;
 import com.janus.janusapp.structs.*;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
- *
  * @author usuario
  */
 public class User implements Serializable {
     public String userName;
+    public String FirstName;
+    public String LastName;
     public String password;
-    String birthDate;
+    LocalDate birthDate;
     String email;
-    public LinkedL<String> ownProjectList=new LinkedL<String>();
-    public LinkedL<String> followedProjects=new LinkedL<String>();
+    public LinkedL<String> ownProjectList = new LinkedL<String>();
+    public LinkedL<String> followedProjects = new LinkedL<String>();
 
-    
-    public User(String name, String password, String birthdate, String email){
-        this.userName = name;
+//Poner todos los atributos en el constructor String FirstName, String LastName,
+    public User( String userName, String password, LocalDate birthdate, String email) {
+        //this.FirstName = FirstName;
+        //this.LastName = LastName;
+        this.userName = userName;
         this.password = password;
         this.birthDate = birthdate;
         this.email = email;
     }
-    public User(){
+
+    public User() {
 
     }
 
-    
-    public void followProject(Project project){
+
+    public void followProject(Project project) {
         followedProjects.append(project.name);
         project.followers.append(this.userName);
     }
-    
-    public void createProject(String name, LinkedL<String> owns, int bdgt,String ctgr,String dscpt){
-        Project newProject = new Project(name, owns, bdgt,ctgr,dscpt);
+
+    public void createProject(String name, LinkedL<String> owns, int bdgt, String ctgr, String dscpt) {
+        Project newProject = new Project(name, owns, bdgt, ctgr, dscpt);
         ownProjectList.append(newProject.name);
     }
 
-    public boolean validateUsers(User userToValidate){
-        if(this.userName.equals(userToValidate.userName) && this.password.equals(userToValidate.password)){
+    public boolean validateUsers(User userToValidate) {
+        if (this.userName.equals(userToValidate.userName) && this.password.equals(userToValidate.password)) {
             return true;
-        } else{
+        } else {
             return false;
         }
     }
 
     @Override
-    public String toString(){
-        String s= "Username: "+userName+" Password: "+password+" Birthdate: "+birthDate+" Email: "+email
-        +" Projects: "+ownProjectList.toString()+" Followed: "+followedProjects.toString();
+    public String toString() {
+        String s = "Username: " + userName + " Password: " + password + " Birthdate: " + birthDate + " Email: " + email
+                + " Projects: " + ownProjectList.toString() + " Followed: " + followedProjects.toString();
         return s;
     }
 }
