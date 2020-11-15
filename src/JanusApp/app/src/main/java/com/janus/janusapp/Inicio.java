fc68de8c7fc1b073aaae6976a05192c8485d2d82
jmorenohj <jmorenoh@unal.edu.co>
2020-11-15 13:35:51 -0500

    Cambios bobos en inicio

--- src/JanusApp/app/src/main/java/com/janus/janusapp/Inicio.java
+++ src/JanusApp/app/src/main/java/com/janus/janusapp/Inicio.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,6 +32,8 @@ import com.janus.janusapp.structs.DynamicArray;
 import com.janus.janusapp.structs.DynamicArrayS;
 import com.janus.janusapp.structs.Trie;
 
+import java.util.ArrayList;
+
 public class Inicio extends FragmentActivity {
     public static Trie<Project> projectTrie;
     public static Trie<User> userTrie;
@@ -45,8 +48,18 @@ public class Inicio extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
-        Food = Software = Technology = Accesories = Art = Entertainment = Services = Science = Education = Other = new DynamicArray<Project>();
+        {Food=new DynamicArray<Project>();
+        Software = new DynamicArray<Project>();
+        Technology = new DynamicArray<Project>();
+        Accesories = new DynamicArray<Project>();
+        Art = new DynamicArray<Project>();
+        Entertainment = new DynamicArray<Project>();
+        Services = new DynamicArray<Project>();
+        Science = new DynamicArray<Project>();
+        Education = new DynamicArray<Project>();
+        Other = new DynamicArray<Project>();}
         String MainUserStr = getIntent().getStringExtra("usuario");
+        Food.append(new Project());
         gson = new Gson();
         MainUser = gson.fromJson(MainUserStr, User.class);
         newProjectFragment frag = new newProjectFragment();
@@ -61,28 +74,35 @@ public class Inicio extends FragmentActivity {
         /**
          * =======================Sección comentada para pruebas del Joselo=====================================
          */
-      /*  firebaseref.child("Users").addValueEventListener(new ValueEventListener() {
+        /*firebaseref.child("Users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
+                Toast.makeText(Inicio.this,"Hols",Toast.LENGTH_LONG).show();
                     for (DataSnapshot ds : snapshot.getChildren()) {
                         User newUser = new User(ds.child("firstName").getValue().toString(),ds.child("lastName").getValue().toString(),
                                 ds.child("email").getValue().toString(),ds.child("username").getValue().toString(),
                                 ds.child("mobileNumber").getValue().toString(),ds.child("password").getValue().toString(),
                                 ds.child("birthDate").getValue().toString(),ds.child("gender").getValue().toString());
-                        newUser.ownProjectList=new DynamicArrayS((String[])ds.child("ownProjectList").getValue());
-                        newUser.followedProjects= new DynamicArrayS((String[])ds.child("followedProjects").getValue());
-                        userTrie.addWord(newUser.username,newUser);
+                        ArrayList<String> ownProjectArrayList = (ArrayList<String>) ds.child("ownProjectList").getValue();
+                        ArrayList<String> followedProjectsArrayList= (ArrayList<String>) ds.child("followedProjects").getValue();
+                        DynamicArrayS ownProjectList = new DynamicArrayS((String[]) ownProjectArrayList.toArray(new String[0]));
+                        //DynamicArrayS followedProjects = new DynamicArrayS((String[]) followedProjectsArrayList.toArray(new String[0]));
+                        //newUser.ownProjectList= ownProjectList;
+                        //newUser.followedProjects= followedProjects;
+                       // if(ds.child("picture").exists()){
+                         //   newUser.picture=ds.child("picture").getValue().toString();
+                        //}
+                        //userTrie.addWord(newUser.username,newUser);
                     }
                 }
-            }
+
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         }); //Trae usuarios de la BD y los mete a un trie
-        firebaseref.child("Project").addValueEventListener(new ValueEventListener() {
+        /*firebaseref.child("Project").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()){
